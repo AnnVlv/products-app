64f74e57a1954827855f47bdb4f0a9a04d6e02da
AnnVlv <anna.valyaeva17@gmail.com>
2021-04-26 17:29:49 +0300

    changes

--- src/app/core/services/toast.service.ts
+++ src/app/core/services/toast.service.ts
@@ -1,27 +1,23 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
-import {debounceTime, filter, withLatestFrom} from 'rxjs/operators';
+import {debounceTime, tap} from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
+  showMessageEvent$: Subject<string> = new Subject<string>();
   message$: Subject<string> = new Subject<string>();
-  private clear$: Subject<null> = new Subject<null>();
 
   constructor() {
-    this.clear$.pipe(
-      withLatestFrom(this.message$),
-      filter(([_, message]) => !!message),
+    this.showMessageEvent$.pipe(
+      tap(message => this.message$.next(message)),
       debounceTime(3000)
-    ).subscribe(() => {
-      this.message$.next('');
-    });
+    ).subscribe(_ => this.message$.next(''));
   }
 
   show(message: string): void {
-    this.message$.next(message);
-    this.clear$.next();
+    this.showMessageEvent$.next(message);
   }
 }
